021b715878a31a005ff02b6f951ddfe8bc55e173
soulforsoup <justintmk@gmail.com>
2022-11-10 11:36:08 +0800

    Initial

---
+++ story_content/user.js
@@ -0,0 +1,62 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6UXx3Kxv5Vr":
+        Script1();
+        break;
+      case "6c5xj9qp5h0":
+        Script2();
+        break;
+      case "66qEyNaJWC1":
+        Script3();
+        break;
+      case "5zVOY5oMf52":
+        Script4();
+        break;
+      case "5YvjItGuwqm":
+        Script5();
+        break;
+  }
+}
+
+function Script1()
+{
+  //mute/unmute
+// Use the global DS object if it exists, otherwise try require:
+var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script2()
+{
+  //mute/unmute
+// Use the global DS object if it exists, otherwise try require:
+var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script3()
+{
+  //mute/unmute
+// Use the global DS object if it exists, otherwise try require:
+var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script4()
+{
+  //mute/unmute
+// Use the global DS object if it exists, otherwise try require:
+var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
+function Script5()
+{
+  //mute/unmute
+// Use the global DS object if it exists, otherwise try require:
+var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume();
+}
+
